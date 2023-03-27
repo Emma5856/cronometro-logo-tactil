130acedfde64b1b50a2ae82118599e231378670b
Emma5856 <126062132+Emma5856@users.noreply.github.com>
2023-03-27 09:22:04 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+let time = 0
+let start = 0
+input.onLogoEvent(TouchButtonEvent.Released, function () {
+    time = input.runningTime() - start
+    basic.showNumber(Math.idiv(time, 1000))
+    basic.clearScreen()
+    basic.showNumber(time % 1000)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    start = input.runningTime()
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+})
